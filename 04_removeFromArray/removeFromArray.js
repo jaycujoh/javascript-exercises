6027b91b94552d30df0a96f16d50f7c5a9fdca2b
jaycujoh <jamie.bellpl@gmail.com>
2025-02-05 16:30:11 +0100

    04_removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    const numArr = []
+
+    //use foreach method to go through each item in the array
+    array.forEach((item) => {
+        //if the arguments does not match any of the other arguments passed in then push to new array
+    if (!args.includes(item)) {
+        numArr.push(item);
+    }
+});
+    //return the new array
+    return numArr;
+
 };
 
 // Do not edit below this line
