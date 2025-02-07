780be09c4147d18f11b55fe1a5f145efdc2843ef
jaycujoh <jamie.bellpl@gmail.com>
2025-02-07 17:33:13 +0100

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+//formula to convert celsius into fahrenheit = (0°C × 9/5) + 32 = 32°F
+//formula to convert fahrenheit into celsiu = (32°F − 32) × 5/9 = 0°C
+
+//math round to round the number to the nearest decimal place
+//
+
+const convertToCelsius = function(fahren) {
+  
+  return Math.round((fahren - 32) * (5 / 9) * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+
+  return Math.round(((cels * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
