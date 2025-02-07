c8e42b9f4eba88703febeb099b4e43129af9e424
jaycujoh <jamie.bellpl@gmail.com>
2025-02-07 16:38:45 +0100

    06_leapYears complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    const century = year % 100 === 0;
+
+    if (year % 4 === 0 && !century || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
